c68ca3ed495d23062041759802afbfac0fde4a31
David Ospina <jdospin@hotmail.com>
2019-05-09 12:15:22 -0400

    Generated component dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+
+const routes: Routes = [
+        { path: 'heroes', component: HeroesComponent }
+    ];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    // The forRoot() method supplies the service providers
+    // and directives needed for routing, and performs the initial navigation based on the current browser URL
+    imports: [ RouterModule.forRoot(routes)],
+    exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { AppRoutingModule } from './app-routing.module';
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
