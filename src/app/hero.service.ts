cad93076bdc987bdb1ac59c7a72c1a1c71b82259
David Ospina <jdospin@hotmail.com>
2019-05-09 10:13:27 -0400

    Created messages component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,8 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    // of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes
+    return of(HEROES);
   }
 }
